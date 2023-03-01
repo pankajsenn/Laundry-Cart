e3b385a147956d34a788957f392b23efc8b5feaa
Pankaj Sen <pankajsenthenking11@gmail.com>
2023-03-01 18:53:16 +0530

    uncommented lines 4,6,9,11

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const cors = require("cors");
 const app = express();
-// const User = require('../src/routes/user');
+const User = require('../src/routes/user');
 const Products = require('../src/routes/product');
-// const Orders = require('../src/routes/order');
+const Orders = require('../src/routes/order');
 app.use(express.json());
 app.use(cors());
-// app.use("/",User);
+app.use("/",User);
 app.use("/",Products);
-// app.use("/",Orders);
+app.use("/",Orders);
 
 module.exports = app;
