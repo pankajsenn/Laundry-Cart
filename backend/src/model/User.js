9890f157ed7cf1ab70060f3f48707bddf462faee
Pankaj Sen <pankajsenthenking11@gmail.com>
2023-03-05 19:44:51 +0530

    registration routes added

---
+++ backend/src/model/User.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const AddressSchema = mongoose.Schema({
+   address:{type:String,required:true},
+   city:{type:String,required:true},
+   pincode:{type:Number,required:true},
+   state:{type:String,required:true},
+})
+
+const UserSchema = mongoose.Schema({
+   name:{type:String ,required:true},
+   password:{type:String ,required:true},
+   email:{type:String ,required:true},
+   phone:{type:String ,required:true},
+   token:{type:String},
+   address:[AddressSchema]
+})
+
+const User = mongoose.model("User",UserSchema);
+module.exports = User;
