02bcaf8b97ad09289437e8932cb1dafc86c2b990
Pankaj Sen <pankajsenthenking11@gmail.com>
2023-03-05 22:38:35 +0530

    register updated

--- backend/src/routes/register.js
+++ backend/src/routes/register.js
@@ -5,14 +5,37 @@ const User = require("../model/User");
 
 router.post(
   "/register",
-  body("name").isLength({ min: 5 }),
-  body("email").isEmail(),
-  body("password").isString({ min: 6 }),
-  body("phone").isLength({ max: 10, min: 10 }),
-  body("address").isLength({ min: 5 }),
-  body("state").isLength({ min: 3 }),
-  body("pincode").isLength({ min: 6, max: 6 }),
-  body("district").isLength({ min: 3 }),
+  body("name")
+  .isAlpha()
+  .withMessage("Name should be Alpha")
+  .isLength({ min: 3 })
+  .withMessage("Name should be greater then 2"),
+  body("email")
+  .isEmail()
+  .withMessage("Its not a valid email"),
+  body("password")
+  .matches(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/)
+  .withMessage('Password must contain at least 8 characters including one letter and one number')
+  .isString({ min: 6 }),
+  body("phone")
+  .isNumeric()
+  .withMessage("Phone number should be number")
+  .isLength({ max: 10, min: 10 })
+  .withMessage("Phone number length should be 10 digit"),
+  body("address")
+  .isString()
+  .withMessage("Address should be string")
+  .isLength({ min: 5 }),
+  body("state")
+  .isLength({ min: 3 }),
+  body("pincode")
+  .isNumeric()
+  .withMessage("Pincode should not be alphanumeric")
+  .isLength({ min: 6, max: 6 })
+  .withMessage("length of pincode should be 6 digit number"),
+  body("district")
+  .isLength({min: 3})
+  .withMessage("District name should not be less then 3"),
   async (req, res) => {
     try {
       const errors = validationResult(req);
@@ -20,6 +43,7 @@ router.post(
         return res.status(400).json({
           status: "failed",
           message: "invalid  registeration",
+          errors: errors.array()
         });
       }
       const {
@@ -73,4 +97,5 @@ router.post(
     }
   }
 );
+
 module.exports = router;
